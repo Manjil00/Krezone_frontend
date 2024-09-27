e8526f379a79aa591901eb7a51f169042f55838e
ACER <210220@softwarica.edu.np>
2024-09-27 21:02:10 +0545

    Section1

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import Section1 from "./components/Section1";
 function App() {
   return (
   <div className="maincontainer h-full w-full">
-    
+
   <Nav/>
   <Section1/>
+  
+  <div className="Section2">
 
   </div>
-
+  
+  </div>
   );
 }
 
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 
 const Section1 = () => {
 return (
-    <div className="Section1 h-auto w-full mt-3 bg-white relative z-0 ">
+
+<div className="Section1 h-auto w-full mt-3 md:mt-8 bg-white relative z-0 ">
     <img src="../images/image 117.png" className="absolute inset-0 h-full w-full object-cover z-0" alt="bg"/>
 
-    <div className="herosection flex  justify-evenly items-center relative z-10">
+    <div className="herosection grid grid-cols-1 md:grid-cols-3 relative z-10">
 
-    <div className="leftSection flex flex-col justify-start items-center gap-3">
+    <div className="leftSection flex flex-col justify-start items-start ml-16 mt-5 gap-5">
         <h1 className="header font-Mona font-bold text-md md:text-4xl text-start">Your Partner at <br/> Disability</h1>
-        <p className="p font-Mona font-normal text-xs md:text-xl ml-4">Impact care seeks to provide good quality services for <br/>people with disabilities, ensuring that their individual <br/>requirements are met</p>
+        <p className="p font-Mona font-normal text-xs md:text-xl">Impact care seeks to provide good quality services for <br/>people with disabilities, ensuring that their individual <br/>requirements are met</p>
         <button className="appointment bg-slate-700 text-white p-2 rounded-lg text-xs md:text-xl ">Book an Appointment</button>
     </div>
 
     <div className="rightSection flex">
         <div className="heroimg">
-        <img src="../images/hero photo.png" className="bg-white p-3" alt="herophoto"/>
+        <img src="../images/hero photo.png" className="bg-white ml-60  p-3 md:block hidden h-auto w-auto z-20" alt="herophoto"/>
+        <img src="../images/mobileframe.png" className="bg-white ml-5 p-3 md:hidden block h-auto w-auto z-20" alt="herophoto"/>
         <img src="../icons/Component20.png" className="absolute bottom-40 -right-1 h-[30px] w-[30px] md:h-auto md:w-auto" alt="logoman"/>
         </div>
     </div>
